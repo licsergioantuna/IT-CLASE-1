c87c9fdef54307ae9c9591ef011c7ebfd23a5522
licsergioantuna <124640783+licsergioantuna@users.noreply.github.com>
2023-02-11 13:08:37 -0300

    Add files via upload

---
+++ Clase 1/JS/Variables.js
@@ -0,0 +1,8 @@
+/* para declarar una variable utilizamos la palabra reservada "let" */
+
+let numero = 62348;
+let frase = 'hola mundo';
+
+/* mostramos los valores de variables en el documento */
+
+document.write (numero);
